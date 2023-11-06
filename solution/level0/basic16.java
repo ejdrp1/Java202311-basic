7b4f7f025253be2a5ab761e2db4e102903bb4aea
ejdrp1 <ejdrp1@naver.com>
2023-11-06 15:31:23 +0900

    문자열 뒤집기

---
+++ solution/level0/basic16.java
@@ -0,0 +1,24 @@
+package level0;
+
+public class basic16 {
+    public String solution(String my_string) {
+
+        String answer = "";
+
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.substring(i, i + 1);
+        }
+
+        return answer;
+    }
+
+//    StringBuilder
+    public String solution1(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+
+        return sb.toString();
+    }
+
+}
