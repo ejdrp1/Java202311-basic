13d1273abf571daa1f3ff7b43db09ca55eb2cb15
ejdrp1 <ejdrp1@naver.com>
2023-11-06 15:31:15 +0900

    배열 뒤집기

---
+++ solution/level0/basic15.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class basic15 {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[num_list.length - i - 1];
+        }
+
+        return answer;
+    }
+}
