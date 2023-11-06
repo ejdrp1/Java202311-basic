03698a22d45ee93ea18455908c5beaae5aa84b64
ejdrp1 <ejdrp1@naver.com>
2023-11-06 15:29:35 +0900

    두 수의 나눗셈

---
+++ solution/level0/basic08.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class basic08 {
+    public int solution(int num1, int num2) {
+        double answer = 0;
+        if ((0 < num1 && num1 <= 100) && (0 < num2 && num2 <= 100)) {
+            answer = (double) num1 / num2 * 1000;
+        }
+        return (int) answer;
+    }
+}
