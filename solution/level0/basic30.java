c6d7dcd7b2a59e5b84fd0c1a488d5614d530e2e3
ejdrp1 <ejdrp1@naver.com>
2023-11-09 15:32:46 +0900

    옷가게 할인 5% 할인 == obj * 0.95 또는 obj - obj * 0.05

---
+++ solution/level0/basic30.java
@@ -0,0 +1,22 @@
+package level0;
+
+public class basic30 {
+    public int solution(int price) {
+        int answer = 0;
+        if (price >= 500000) {
+            answer = (int) (price * 0.8);
+        } else if (price >= 300000) {
+            answer = (int) (price * 0.9);
+        } else if (price >= 100000) {
+            answer = (int) (price * 0.95);
+        } else  {
+            answer = price;
+        }
+        return answer;
+    }
+
+//    다른 풀이
+    public int solution1(int price) {
+        return (int) (price >= 500000 ? price - price * 0.2 : price >= 300000 ? price - price * 0.1 : price >= 100000 ? price - price * 0.05 : price);
+    }
+}
