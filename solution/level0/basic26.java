33ce21546c02540164c58c2321d9085b65f214f7
ejdrp1 <ejdrp1@naver.com>
2023-11-09 14:46:58 +0900

    순서쌍의 개수

---
+++ solution/level0/basic26.java
@@ -0,0 +1,16 @@
+package level0;
+
+public class basic26 {
+    public int solution(int n) {
+
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
