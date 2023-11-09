90a022cad7ce7daba696703e3c70595f849afd3f
ejdrp1 <ejdrp1@naver.com>
2023-11-09 14:59:53 +0900

    제곱수 판별하기 Math.sqrt()

---
+++ solution/level0/basic28.java
@@ -0,0 +1,12 @@
+package level0;
+
+public class basic28 {
+    public int solution(int n) {
+
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
+
+    }
+
+//    Math.sqrt() : 특정값의 제곱근(루트)을 구하는 메소드
+//    ex) Math.sqrt(144) : 12;
+}
