398bdb8051e327dba4b318bd15d68eab6a82f93a
ejdrp1 <ejdrp1@naver.com>
2023-11-11 12:10:36 +0900

    시작 및 종료하기 메서드 포지션 번호 선택 메서드

--- solution/level0/play/PositionInfoService/Position.java
+++ solution/level0/play/PositionInfoService/Position.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Position {
     public static Scanner scanner = new Scanner(System.in);
+
     public void startPositionInfoService() {
 
         System.out.println("안녕하세요. 축구 포지션 정보 서비스입니다!");
@@ -12,18 +13,36 @@ public class Position {
         selectStartingNum(selectStartingNum);
 
 
-
-
     }
 
     public void selectStartingNum(int selectStartingNum) {
         if (selectStartingNum == 1) {
-            System.out.println("궁금하신 포지션을 입력해주세요!");
-            System.out.println("1.공격수 2.미드필더 3.수비수 4.골키퍼");
+            selectPositionTypeText();
         } else {
             System.out.println("이용해주셔서 감사합니다! ( _ _ )");
             return;
         }
     }
+
+    public void selectPositionTypeText() {
+        System.out.println("궁금하신 포지션을 입력해주세요!");
+        System.out.println("1.공격수 2.미드필더 3.수비수 4.골키퍼");
+        int selectPositionTypeNum = scanner.nextInt();
+        selectPositionTypeNum(selectPositionTypeNum);
+    }
+
+    private void selectPositionTypeNum(int selectPositionTypeNum) {
+        if (selectPositionTypeNum > 0 && selectPositionTypeNum < 5) {
+            switch (selectPositionTypeNum) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+            }
+        }
+        System.out.println("옳바르지 않은 번호입니다. 다시 입력해주세요");
+    }
+
+//    public void
 
 }
