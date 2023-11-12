423cf7a0626a4084672da8122ff2c89822308304
ejdrp1 <ejdr1@naver.com>
2023-11-12 22:02:27 +0900

    Salary 클래스

--- solution/level0/play/PlayerSalaryCalculator/Model/Salary.java
+++ solution/level0/play/PlayerSalaryCalculator/Model/Salary.java
@@ -2,6 +2,29 @@ package level0.play.PlayerSalaryCalculator.Model;
 
 public class Salary {
 
+    private String weeklyPay;
+    private String annualIncome;
 
+    public Salary() {}
 
+    public Salary(String weeklyPay, String annualIncome) {
+        this.weeklyPay = weeklyPay;
+        this.annualIncome = annualIncome;
+    }
+
+    public String getWeeklyPay() {
+        return weeklyPay;
+    }
+
+    public void setWeeklyPay(String weeklyPay) {
+        this.weeklyPay = weeklyPay;
+    }
+
+    public String getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(String annualIncome) {
+        this.annualIncome = annualIncome;
+    }
 }
