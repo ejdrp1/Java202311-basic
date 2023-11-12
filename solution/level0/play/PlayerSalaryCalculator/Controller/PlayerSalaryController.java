9b49755fe8c638191c40501e551293d3ee07a613
ejdrp1 <ejdr1@naver.com>
2023-11-12 22:31:08 +0900

    PlayerSalaryView startPlayerSalaryCalculator()

--- solution/level0/play/PlayerSalaryCalculator/Controller/PlayerSalaryController.java
+++ solution/level0/play/PlayerSalaryCalculator/Controller/PlayerSalaryController.java
@@ -1,4 +1,13 @@
 package level0.play.PlayerSalaryCalculator.Controller;
 
+import java.util.Scanner;
+
 public class PlayerSalaryController {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public void inputPlayerName(String inputPlayerName) {
+        System.out.println("'" + inputPlayerName + "'" + "선수의 급여를 입력해주세요.");
+    }
+
 }
--- solution/level0/play/PlayerSalaryCalculator/View/PlayerSalaryView.java
+++ solution/level0/play/PlayerSalaryCalculator/View/PlayerSalaryView.java
@@ -1,8 +1,19 @@
 package level0.play.PlayerSalaryCalculator.View;
 
+import level0.play.PlayerSalaryCalculator.Controller.PlayerSalaryController;
+
+import java.util.Scanner;
+
 public class PlayerSalaryView {
 
+    PlayerSalaryController playerSalaryController = new PlayerSalaryController();
     public void startPlayerSalaryCalculator() {
+        System.out.println("입력하신 선수의 급여를 계산하는 프로그램입니다!");
+        System.out.println("선수의 이름과 주급 또는 연봉을 입력해주세요.");
+        System.out.print("선수 이름 : ");
+        playerSalaryController.inputPlayerName(playerSalaryController.scanner.next());
+
+
 
     }
 }
