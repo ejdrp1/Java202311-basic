7ebbba25509cf79aeb9cb38158ec87c95cf54a08
ejdrp1 <ejdrp1@naver.com>
2023-11-14 13:18:05 +0900

    평균 구하기

---
+++ solution/level1/basic/basic04.java
@@ -0,0 +1,23 @@
+package level1.basic;
+
+import java.util.Arrays;
+
+public class basic04 {
+    public double solution(int[] arr) {
+
+        double answer = 0;
+        double sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            answer = sum / arr.length;
+        }
+
+        return answer;
+    }
+
+//    다른 풀이
+    public double solution1(int[] arr) {
+        return Arrays.stream(arr).average().getAsDouble();
+    }
+}
