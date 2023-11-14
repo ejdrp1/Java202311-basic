fdf0718a8fa6c75f2ad1860164d1887de369b360
ejdrp1 <ejdrp1@naver.com>
2023-11-14 15:55:43 +0900

    문자열을 정수로 바꾸기

---
+++ solution/level1/basic/basic07.java
@@ -0,0 +1,9 @@
+package level1.basic;
+
+public class basic07 {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
