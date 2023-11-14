585fb54de88e85beb04141c00df4c70b9444d3ca
ejdrp1 <ejdrp1@naver.com>
2023-11-14 19:50:38 +0900

    김서방~~~~~~~~ 어딨어~~~~~~~~~~

---
+++ solution/level1/basic/basic15.java
@@ -0,0 +1,15 @@
+package level1.basic;
+
+public class basic15 {
+    public String solution(String[] seoul) {
+
+        String answer = "";
+
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+            }
+        }
+        return answer;
+    }
+}
