fa0c11d0ff698c89d1eba85f2a2165dc943739e0
ejdrp1 <ejdrp1@naver.com>
2023-11-14 12:22:15 +0900

    짝수와 홀수 파일 경로 수정

---
+++ solution/level1/basic/basic02.java
@@ -0,0 +1,9 @@
+package level1.basic;
+
+public class basic02 {
+    public String solution(int num) {
+        String odd = "Odd";
+        String even = "Even";
+        return (num % 2 == 0) ? even : odd;
+    }
+}
