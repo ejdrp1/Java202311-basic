7b326cf77e68eeba53486a3bf49def55133ef3f9
ejdrp1 <ejdrp1@naver.com>
2023-11-15 15:26:52 +0900

    미니 리그로 변경 프로그램 시작 / 종료 메서드

---
+++ solution/variableMethod/basic/teamOfRankingCalculator/Ranking.java
@@ -0,0 +1,9 @@
+package variableMethod.basic.teamOfRankingCalculator;
+
+public class Ranking extends Team {
+
+
+    public Ranking(int winMatches, int drawMatches, int loseMatches, int matches, int score, int losePoint, int differenceBetweenGainsAndLosses) {
+        super(winMatches, drawMatches, loseMatches, matches, score, losePoint, differenceBetweenGainsAndLosses);
+    }
+}
--- solution/variableMethod/basic/teamOfRankingCalculator/TeamRankingController.java
+++ solution/variableMethod/basic/teamOfRankingCalculator/TeamRankingController.java
@@ -1,4 +1,26 @@
 package variableMethod.basic.teamOfRankingCalculator;
 
+import java.util.Scanner;
+
 public class TeamRankingController {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void startProgram() {
+        TeamRankingView.startTeamRankingCalculator();
+        TeamRankingView.inputSelectStartProgramNum();
+        int selectStartingNum = scanner.nextInt();
+        selectStartingNum(selectStartingNum);
+    }
+
+    private void selectStartingNum(int selectNum) {
+        switch (selectNum) {
+            case 1:
+                TeamRankingView.inputTeamInfo();
+                break;
+            case 2:
+                TeamRankingView.endProgram();
+                return;
+        }
+    }
 }
