be220bdbc3dd03a54811a682fcfa5990a49438e5
ejdrp1 <ejdrp1@naver.com>
2023-11-20 12:08:49 +0900

    index reverse()

--- solution/groupStageCalculator/GroupStageController.java
+++ solution/groupStageCalculator/GroupStageController.java
@@ -45,30 +45,42 @@ public class GroupStageController {
         TeamIndexNum teamIndexNum = new TeamIndexNum();
         int[] teamIndex = {0, 1, 2, 3};
         teamIndexNum.setTeamIndexNum(teamIndex);
-        System.out.println(Arrays.toString(teamIndexNum.getTeamIndexNum()));
 
 
-//        GroupStageView.introSetGroupStageTeamInfo(groupA); // 조 편성 msg
-//        repeatGroupStageProgress(groupA, teamMatchRecord);
+        GroupStageView.introSetGroupStageTeamInfo(groupA, teamIndex); // 조 편성 msg
+        repeatGroupStageProgress(groupA, teamMatchRecord, teamIndex);
     }
 
-    private void repeatGroupStageProgress(List<Team> groupA, TeamMatchRecord teamMatchRecord) {
+    private void repeatGroupStageProgress(List<Team> groupA, TeamMatchRecord teamMatchRecord, int[] teamIndex) {
         GroupStageView.groupStageProgressInfo(groupA, teamMatchRecord); // 경기 진행 상황
-        GroupStageView.introMatchesScheduleInfo(groupA); // 경기 일정 msg
+//        GroupStageView.introMatchesScheduleInfo(groupA); // 경기 일정 msg
         for (int i = 0; i < 6; i++) {
-            groupStageProgress(groupA, teamMatchRecord);
+            groupStageProgress(groupA, teamMatchRecord, teamIndex);
         }
     }
 
-    private void groupStageProgress(List<Team> groupA, TeamMatchRecord teamMatchRecord) {
-        GroupStageView.nextMatchesInfo(); // 경기 진행 msg
-        inputTeamScore(groupA, teamMatchRecord);
+    private void groupStageProgress(List<Team> groupA, TeamMatchRecord teamMatchRecord, int[] teamIndex) {
+//        GroupStageView.nextMatchesInfo(); // 경기 진행 msg
+        inputTeamScore(groupA, teamMatchRecord, teamIndex);
     }
 
-    private void inputTeamScore(List<Team> groupA, TeamMatchRecord teamMatchRecord) {
+    private void inputTeamScore(List<Team> groupA, TeamMatchRecord teamMatchRecord, int[] teamIndex) {
 //      1. 팀 스코어 입력
         teamMatchRecord.incrementMatchesNum(); // 경기수 증가
-        inputTeamScoreNum(groupA); // 팀 스코어 입력
+
+        teamIndexFirstChange(teamIndex);
+
+
+
+
+
+
+
+
+
+
+//        inputTeamScoreNum(groupA, teamIndex); // 팀 스코어 입력
+
 
 
 //      2. 경기 승 무 패 count 조건식 + 득실차 + 승점
@@ -76,12 +88,22 @@ public class GroupStageController {
 //        endMatchesRound(teamMatchRecord); // 경기 종료 후
     }
 
-    private void inputTeamScoreNum(List<Team> groupA) {
-        System.out.println("inputTeamScoreNum");
-        GroupStageView.inputFirstMatchesScoreNumInfo(groupA);
+    private void teamIndexFirstChange(int[] teamIndex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < teamIndex.length; i++) {
+            sb.append(teamIndex[i]).append(" ");
+        }
+        String teamIndexStr = sb.reverse().toString(); // 3 2 1 0
+    }
+
+
+    private void inputTeamScoreNum(List<Team> groupA, int[] teamIndex) {
+        GroupStageView.inputFirstMatchesScoreNumInfo(groupA, teamIndex);
         String firstTeamScore = scanner.next();
-        GroupStageView.inputSecondMatchesScoreNumInfo(groupA);
+        GroupStageView.inputSecondMatchesScoreNumInfo(groupA, teamIndex);
         String secondTeamScore = scanner.next();
+
+
     }
 
 //    private void endMatchesRound(TeamMatchRecord teamMatchRecord) {
