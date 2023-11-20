74f6468b42576dfa4607aab3c5e47588ee560cc6
ejdrp1 <ejdrp1@naver.com>
2023-11-20 14:03:42 +0900

    제발

--- solution/groupStageCalculator/GroupStageController.java
+++ solution/groupStageCalculator/GroupStageController.java
@@ -67,8 +67,12 @@ public class GroupStageController {
     private void inputTeamScore(List<Team> groupA, TeamMatchRecord teamMatchRecord, int[] teamIndex) {
 //      1. 팀 스코어 입력
         teamMatchRecord.incrementMatchesNum(); // 경기수 증가
+//        teamIndexFirstChange(teamIndex); // 0 1 2 3 => 3 2 1 0
+//        teamIndexSecondChange(teamIndex); // 3 2 1 0 => 0 2 1 3
+//        teamIndexSecondChangeReverse(teamIndex); // 0 2 1 3 => 3 1 2 0
 
-        inputTeamScoreNum(groupA, teamMatchRecord, teamIndex); // 팀 스코어 입력
+
+//        inputTeamScoreNum(groupA, teamMatchRecord, teamIndex); // 팀 스코어 입력
 
 
 //      2. 경기 승 무 패 count 조건식 + 득실차 + 승점
@@ -77,42 +81,52 @@ public class GroupStageController {
     }
 
 
+    private void teamIndexSecondChangeReverse(int[] teamIndex) { // 3 1 2 0
+        teamIndex[1] = 2;
+        teamIndex[2] = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < teamIndex.length; i++) {
+            sb.append(teamIndex[i]).append(" ");
+        }
+        String teamIndexStr = sb.reverse().toString();
+    }
+
+    private void teamIndexSecondChange(int[] teamIndex) { // 0 2 1 3
+        teamIndex[1] = 2;
+        teamIndex[2] = 1;
+    }
+
+    private void teamIndexFirstChange(int[] teamIndex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < teamIndex.length; i++) {
+            sb.append(teamIndex[i]).append(" ");
+        }
+        String teamIndexStr = sb.reverse().toString(); // 3 2 1 0
+    }
+
     private void inputTeamScoreNum(List<Team> groupA, TeamMatchRecord teamMatchRecord, int[] teamIndex) {
         GroupStageView.inputFirstMatchesScoreNumInfo(groupA, teamIndex);
         String firstTeamScore = scanner.next();
         GroupStageView.inputSecondMatchesScoreNumInfo(groupA, teamIndex);
         String secondTeamScore = scanner.next();
-        teamScoreNumCalculator(firstTeamScore, secondTeamScore, teamMatchRecord, teamIndex, groupA);
+//        teamScoreNumCalculator(firstTeamScore, secondTeamScore, teamMatchRecord, teamIndex, groupA);
     }
 
-    private void teamScoreNumCalculator(String firstTeamScore, String secondTeamScore, TeamMatchRecord teamMatchRecord, int[] teamIndex, List<Team> groupA) {
-//        1. String => int 변환
-        int[] firstScoreNum = new int[firstTeamScore.length() - 1];
-        int[] secondScoreNum = new int[secondTeamScore.length() - 1];
-//        2. 정수형 배열에 담기
-        String[] firstScoreArr = firstTeamScore.split(",");
-        String[] secondScoreArr = secondTeamScore.split(",");
-        for (int i = 0; i < firstScoreArr.length; i++) {
-            firstScoreNum[i] = Integer.parseInt(firstScoreArr[i]);
-            secondScoreNum[i] = Integer.parseInt(secondScoreArr[i]);
-        }
-//        3. index 값 비교
-        if (firstScoreNum[0] > firstScoreNum[1]) { // 첫번째팀이 승리한다면
-//            1. firstScoreNum[0] == 득점을 누적
-        }
-
-
-    }
-
-
-//    private void teamIndexFirstChange(int[] teamIndex) {
-//        teamIndexFirstChange(teamIndex);
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < teamIndex.length; i++) {
-//            sb.append(teamIndex[i]).append(" ");
+//    private void teamScoreNumCalculator(String firstTeamScore, String secondTeamScore, TeamMatchRecord teamMatchRecord, int[] teamIndex, List<Team> groupA) {
+////        1. String => int 변환
+//        int[] firstScoreNum = new int[firstTeamScore.length() - 1];
+//        int[] secondScoreNum = new int[secondTeamScore.length() - 1];
+////        2. 정수형 배열에 담기
+//        String[] firstScoreArr = firstTeamScore.split(",");
+//        String[] secondScoreArr = secondTeamScore.split(",");
+//        for (int i = 0; i < firstScoreArr.length; i++) {
+//            firstScoreNum[i] = Integer.parseInt(firstScoreArr[i]);
+//            secondScoreNum[i] = Integer.parseInt(secondScoreArr[i]);
 //        }
-//        String teamIndexStr = sb.reverse().toString(); // 3 2 1 0
+////        3. index 값 비교
 //    }
+
+
 
 //    private void endMatchesRound(TeamMatchRecord teamMatchRecord) {
 //        teamMatchRecord.incrementMatchesNum(); // 경기수 증가
